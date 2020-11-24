7d33b589b23e3447eda5c291b5872b0516e66591
mattiarossi578@gmail.com <mattiarossi578@gmail.com>
2020-11-24 11:45:04 +0100

    Aggiungere i file di progetto.

---
+++ esercitazione/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace esercitazione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String unicodedString = "Questa stringa Unicode ha 2 caratteri al di fuori di" +
+            // "Intervallo ASCII: \ n" +
+            // "pi (\ u03A0) e Sigma (\ u03A3).";
+            Console.WriteLine(" inserisci una stringa ");
+            string unicode = Console.ReadLine();
+            // Console.WriteLine ("stringa originale:");
+            // Console.OutputEncoding = System.Text.Encoding.Unicode; questa riga di codice è necessaria al fine
+            // di far apparire un video il simbolo che è rappresentato dal codice in unicode
+
+            // Console.WriteLine (unicodedString);
+            // Console.WriteLine ();
+        }
+    }
+}
